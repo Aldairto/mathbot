03e2141b6368afcfdca4b2a04e8faf1dcc1dced8
Aldairto <aldairto@gmail.com>
2025-03-17 22:37:24 -0600

    Pruebas lunes 11

---
+++ components/pdf-download-button.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+// Este archivo es un re-export del componente DownloadPDFButton
+// para mantener compatibilidad con las importaciones existentes
+import DownloadPDFButton from "./download-pdf-button"
+
+export default DownloadPDFButton
+
