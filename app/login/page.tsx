0b69c4353fb3d51f40a65f1434617ebeaa99213b
Aldairto <aldairto@gmail.com>
2025-03-18 18:05:07 -0600

    mejora inicio de sesion 3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -75,7 +75,7 @@ export default function LoginPage() {
         <CardFooter className="flex flex-col space-y-4 pt-0">
           <div className="text-sm text-center">
             ¿Olvidaste tu contraseña?{" "}
-            <Link href="/reset-password" className="text-primary hover:underline">
+            <Link href="/forgot-password" className="text-primary hover:underline">
               Recuperar acceso
             </Link>
           </div>
